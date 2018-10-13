a23e78f94e7cc9a6159ef73f8f12d3b51f593593
SkriiL <flx.grimm@gmail.com>
2018-10-13 13:56:08 +0200

    #3 fix disconnection bug

--- server.py
+++ server.py
@@ -1,21 +1,25 @@
 import socket
 import threading
+import os
 
 
 class Server:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind(("0.0.0.0", 5060))
-        self.sock.listen(1)
+        self.sock.listen(2)
         self.connections = []
 
     def handler(self, c, a):
         while True:
-            data = c.recv(1024)
-            for connection in self.connections:
-                connection.send(data)
-            if not data:
-                break
+            try:
+                data = c.recv(1024)
+                for connection in self.connections:
+                    connection.send(data)
+                if not data:
+                    break
+            except:
+                self.restart()
 
     def run(self):
         while True:
@@ -25,6 +29,9 @@ class Server:
             thread.start()
             self.connections.append(c)
             print(self.connections)
+
+    def restart(self):
+        os.system("end.bat")
 
 
 server = Server()
