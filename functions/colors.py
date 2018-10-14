a75503b2535659fc82f4c2e9dec1476cc5cf45f2
SkriiL <flx.grimm@gmail.com>
2018-10-14 13:01:55 +0200

    #6 fix color bug

--- functions/colors.py
+++ functions/colors.py
@@ -17,7 +17,7 @@ class Colors:
         self.colors_left = [self.blue, self.red, self.green, self.yellow, self.cyan, self.magenta]
 
     def pick(self):
-        i = random.randint(0, len(self.colors))
+        i = random.randint(0, len(self.colors_left))
         color = self.colors_left[i]
         del self.colors_left[i]
         return color
