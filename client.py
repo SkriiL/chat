3667a8d0b4cb02427751e7c0b498eccb58489322
SkriiL <flx.grimm@gmail.com>
2018-10-14 20:47:53 +0200

    first

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 class Client:
     def __init__(self):
@@ -6,3 +7,19 @@ class Client:
 
     def connect(self):
         self.sock.connect(("skriil.ddnss.de", 56789))
+
+    def send(self):
+        msg = input("")
+        self.sock.send(bytes(msg, "utf-8"))
+
+    def recv(self):
+        while True:
+            data = self.sock.recv(1024)
+            if not data:
+                break
+            data = str(data, encoding="utf-8")
+            print(data)
+
+    def run(self):
+        thread = threading.
+        thread.daemon = True
